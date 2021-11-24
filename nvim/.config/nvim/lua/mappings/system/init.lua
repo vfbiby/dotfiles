0f96f176da4ecff8e3a5f799ecab0ae9acb64449
James zhang <liulfbaby@gmail.com>
2021-11-24 17:03:29 +0800

    Add basic tmux config

--- nvim/.config/nvim/lua/mappings/system/init.lua
+++ nvim/.config/nvim/lua/mappings/system/init.lua
@@ -1,7 +1,10 @@
 local map = vim.api.nvim_set_keymap
 map('n', '<space>fs', ':w<cr>', {})
-local opts = { noremap = true }
+local opts = { noremap = true, silent = true }
 map('n', '<c-h>', '<c-w>h', opts)
 map('n', '<c-j>', '<c-w>j', opts)
 map('n', '<c-k>', '<c-w>k', opts)
 map('n', '<c-l>', '<c-w>l', opts)
+
+-- nmap <silent><Space><cr> o<Esc>
+map('n', '<space><cr>', 'o<esc>', opts)
